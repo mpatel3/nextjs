5dbc9e7a891b938d1b3e5f651ce061d76143390d
mpatel3 <manthanp@cybage.com>
2021-02-04 23:03:33 +0530

    getStaticPath to provide all dynamic routes possible values

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,7 +1,31 @@
-// dynamics routes will be name [] for dynamic path.
-// here we need to get details of an individual item and display on the page component for the nextjs to render. 
+/*  Dynamics routes will be name [] for dynamic path.
+    Here we need to get details of an individual item and display on the page component for the nextjs to render. 
+    Need to tell next js about possible routes so that at build time it can generate all the possible routes
+    This is the function which will run at build time. 
+    Inside this function we need to mention what are the possible values our route parameter accepts.
+*/ 
+export const getStaticPaths =  async () => {
+    const results = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await results.json(); // .json is async await that.
+
+    // path property needs to be an array of objects.
+    // each objects reprsents a route. In each object we need to specify route parameter.
+    const paths = users.map(user => {
+        return {
+            params: {id: user.id.toString()}
+        }
+    });
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
 const Details = () => {
-    return ( <div>Details Page</div> );
+    return ( <div>
+                Hello
+            </div> 
+            );
 }
  
 export default Details;
