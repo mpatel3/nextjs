6388ce7e1ff3a12ed736ca78f82721e364c00d6b
Manthan Patel <14mcei14@nirmauni.ac.in>
2021-02-04 22:24:48 +0530

    Update [id].js

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,5 @@
+// dynamics routes will be name [] for dynamic path.
+// here we need to get details of an individual item and display on the page component for the nextjs to render. 
 const Details = () => {
     return ( <div>Details Page</div> );
 }
